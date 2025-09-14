d6eb83db7160e393955602be0bf2d199c5896783
jonfodi <masterfodi@gmail.com>
2025-09-14 15:13:23 -0400

    yo

--- docker/server.js
+++ docker/server.js
@@ -1,23 +1,24 @@
-const { Server } = require('node-hl7-server')
+const { Server } = require("node-hl7-server");
 
-const server = new Server({ bindAddress: '0.0.0.0' })
+const server = new Server({ bindAddress: "0.0.0.0" });
 
 const inbound = server.createInbound({ port: 3000 }, async (req, res) => {
-  await res.sendResponse('AA')
-})
+  console.log("Sending response: AA");
+  await res.sendResponse("AA");
+});
 
-inbound.on('client.close', () => {
-  console.log('Client Disconnected')
-})
+inbound.on("client.close", () => {
+  console.log("Client Disconnected");
+});
 
-inbound.on('client.connect', () => {
-  console.log('Client Connected')
-})
+inbound.on("client.connect", () => {
+  console.log("Client Connected");
+});
 
-inbound.on('data.raw', (data) => {
-  console.log('Raw Data:', data)
-})
+inbound.on("data.raw", (data) => {
+  console.log("Raw Data:", data);
+});
 
-inbound.on('listen', () => {
-  console.log('Ready to Listen for Messages')
-})
+inbound.on("listen", () => {
+  console.log("Ready to Listen for Messages");
+});
